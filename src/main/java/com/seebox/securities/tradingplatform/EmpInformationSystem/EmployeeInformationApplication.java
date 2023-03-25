7eb586b2d1a050a72cb690d7eb7d88dcb200eb19
joshua <deguzman.joshua96@gmail.com>
2023-03-25 20:53:36 +0800

    first commit

---
+++ src/main/java/com/seebox/securities/tradingplatform/EmpInformationSystem/EmployeeInformationApplication.java
@@ -0,0 +1,12 @@
+package com.seebox.securities.tradingplatform.EmpInformationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class EmployeeInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeInformationApplication.class, args);
+	}
+
+}
